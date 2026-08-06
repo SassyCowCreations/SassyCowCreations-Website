f9faa82831a0bd7f0a96102274f99fb0cca35247
SassyCowCreations <sassycowcreations@gmail.com>
2026-08-06 11:31:58 +0100

    Update products.js

--- assets/js/products.js
+++ assets/js/products.js
@@ -50,7 +50,6 @@ window.SASSY_PRODUCTS = [
     price: 3.75,
     category: "Drink Accessories",
     featured: true,
-    badge: "Mix & Match",
     short: "A handmade black Monster-style can tab opener charm.",
     description: "Add a playful finishing touch with this handmade can tab opener charm. Attach it to keys, bags or one of our can holders.",
     images: [
@@ -89,7 +88,6 @@ window.SASSY_PRODUCTS = [
     price: 3.75,
     category: "Drink Accessories",
     featured: true,
-    badge: "Mix & Match",
     short: "A handmade Red Bull-style can tab opener charm.",
     description: "Add a playful finishing touch with this handmade can tab opener charm. Attach it to keys, bags or one of our can holders.",
     images: [
@@ -97,14 +95,27 @@ window.SASSY_PRODUCTS = [
     ]
   },
   {
-    id: "red-bull-can-holder",
+    id: "red-bull-can-holder 250ml",
     name: "Red Bull Original 250ml Can Holder",
     price: 7.95,
     category: "Can Holders",
     featured: true,
-    badge: "Bestseller",
     short: "A handmade holder designed for a 250ml Red Bull can.",
-    description: "Keep your 250ml Red Bull can secure and easy to carry with this handmade can holder. It includes a lid, handle and matching charm.",
+    description: "Keep your 250ml Red Bull can secure and easy to carry with this handmade can holder. It includes a lid, handle and matching cab tab opener.",
+    images: [
+      "assets/images/products/Red Bull 1.png",
+      "assets/images/products/Red Bull 2.png",
+      "assets/images/products/Red Bull 3.png"
+    ]
+  },
+   {
+    id: "red-bull-can-holder 500ml",
+    name: "Red Bull Original 500ml Can Holder",
+    price: 7.95,
+    category: "Can Holders",
+    featured: false,
+    short: "A handmade holder designed for a 500ml Red Bull can.",
+    description: "Keep your 500ml Red Bull can secure and easy to carry with this handmade can holder. It includes a lid, handle and matching can tab opener.",
     images: [
       "assets/images/products/Red Bull 1.png",
       "assets/images/products/Red Bull 2.png",
@@ -116,13 +127,47 @@ window.SASSY_PRODUCTS = [
     name: "Bow Can Cooler",
     price: 12.99,
     category: "Can Holders",
-    featured: true,
-    badge: "Bestseller",
-    short: "A handmade can cooler finished with a decorative bow.",
+    featured: False,
+    short: "A handmade can cooler that comes as a set with a bow straw topper and a bow can tab opener.",
     description: "A handmade can cooler with a decorative bow design. Ideal for keeping your canned drink secure while adding a unique handmade touch.",
     images: [
       "assets/images/products/Bow Cooler 1.png",
       "assets/images/products/Bow Cooler 2.png"
     ]
-  }
-];
+  },
+ {
+    id: "Pepsi Cooler",
+    name: "Pepsi Cooler",
+    price: 12.99,
+    category: "Can Holders",
+    featured: true,
+    badge: "New Item",
+    short: "A handmade can cooler that will come with a matching can tab opener.",
+    description: "A handmade can cooler for every Pepsi fan!. Ideal for keeping your canned drink secure while adding a unique handmade touch.",
+    images: [
+      "assets/images/products/Pepsi Cooler 1.png",
+      "assets/images/products/Pepsi Cooler 2.png",
+    ]
+},
+  {
+    id: "can-tab-grinch",
+    name: "Grinch Can Tab Opener Set",
+    price: 3.75,
+    category: "Drink Accessories",
+    featured: false,
+    short: "A The Grinch drinks-can tab opener charm and straw topper set.",
+    description: "Add a playful finishing touch with this handmade can tab opener and straw topper set. Attach it to keys, bags or one of our can holders.",
+    images: [
+      "assets/images/products/Grinch.png"
+      ]
+},
+  {
+    id: "can-tab-myers",
+    name: "Michael Myers Can Tab Opener Set",
+    price: 3.75,
+    category: "Drink Accessories",
+    featured: false,
+    short: "A horror inspired Michael Myers drinks-can tab opener charm and straw topper set.",
+    description: "Something for the horro lovers! A Michael Myers inspired can tab opener and straw topper set. Attach it to keys, bags or one of our can holders.",
+    images: [
+      "assets/images/products/Michael Myers.png"
